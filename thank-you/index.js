a30f5323b8ff812d4387a7052abcbdb3497087b2
koheimizuno <skygentleman626@gmail.com>
2024-11-26 16:48:33 +0900

    fix issue not for displaying main image

--- thank-you/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const topButton = document.querySelector(".top-button");
-  topButton.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-});
