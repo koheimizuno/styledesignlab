3c139a1226c14a21d4982ae51140a86c4d20325c
koheimizuno <skygentleman626@gmail.com>
2024-11-26 21:02:50 +0900

    implement hero-image animation in top page

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,84 +1,85 @@
-new Swiper(".hero-swiper", {
-  loop: true,
-  effect: "fade",
-  speed: 5000,
-  autoplay: {
-    delay: 2000,
-  },
-  fadeEffect: {
-    crossFade: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-new Swiper(".contents-swiper", {
-  loop: true,
-  autoplay: {
-    delay: 4000,
-  },
-  centeredSlides: true,
-  spaceBetween: 30,
-  speed: 1000,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      spaceBetween: 100,
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".hero-swiper", {
+    direction: "horizontal",
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
     },
-  },
-});
-
-new Swiper(".works-swiper", {
-  loop: true,
-  autoplay: {
-    delay: 4000,
-  },
-  centeredSlides: false,
-  spaceBetween: 30,
-  slidesPerView: "auto",
-  speed: 1000,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    768: {
-      spaceBetween: 100,
+    speed: 2000,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
     },
-  },
-});
-
-new Swiper(".topics-swiper", {
-  loop: true,
-  autoplay: {
-    delay: 4000,
-  },
-  centeredSlides: false,
-  spaceBetween: 30,
-  slidesPerView: "auto",
-  speed: 1000,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    768: {
-      spaceBetween: 100,
+  });
+  new Swiper(".contents-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
     },
-  },
+    centeredSlides: true,
+    spaceBetween: 30,
+    speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 100,
+      },
+    },
+  });
+
+  new Swiper(".works-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    centeredSlides: false,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 100,
+      },
+    },
+  });
+
+  new Swiper(".topics-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    centeredSlides: false,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 100,
+      },
+    },
+  });
 });
