2788bbe3de8b903dd5e5900debb22a15ca06aee1
koheimizuno <skygentleman626@gmail.com>
2024-11-27 02:55:37 +0900

    fix scroll animation of header

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,6 @@
+let lastScrollTop = 0;
+let header = $("header");
+
 $(document).ready(function () {
   $(".hamburger-btn").click(() => {
     $(".hamburger-sec").css("transform", "translateX(0)");
@@ -13,5 +16,35 @@ $(document).ready(function () {
     $(".category-btn").toggleClass("change");
   });
 
-  gsap.registerPlugin(ScrollTrigger);
+  $(window).on("scroll", function () {
+    let scrollTop = $(this).scrollTop();
+    if (scrollTop > lastScrollTop) {
+      if ($(window).width() > 768) {
+        gsap.to(header.find("nav"), { padding: "24px 0 18px", duration: 0.5 });
+      } else {
+        gsap.to(header.find("nav"), {
+          padding: "16px 16px 12px",
+          duration: 0.5,
+        });
+        gsap.to(header.find(".logo"), {
+          height: "50px",
+          duration: 0.5,
+        });
+      }
+    } else if (scrollTop < lastScrollTop) {
+      if ($(window).width() > 768) {
+        gsap.to(header.find("nav"), { padding: "80px 0 64px", duration: 0.5 });
+      } else {
+        gsap.to(header.find("nav"), {
+          padding: "24px 16px 18px",
+          duration: 0.5,
+        });
+        gsap.to(header.find(".logo"), {
+          height: "70px",
+          duration: 0.5,
+        });
+      }
+    }
+    lastScrollTop = scrollTop;
+  });
 });
